dd08b002b62c48a92f3bb837ad0bd57fc14a2112
RuchirDixit <ruchirtd96@gmail.com>
2021-05-16 20:03:38 +0530

    [Ruchir]Added: Post API for add user

---
+++ src/main/java/com/example/demo/controller/PostRestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+
+@RestController
+@RequestMapping("/post")
+public class PostRestController {
+
+	@PostMapping("/addUser")
+	public String addUser(@RequestBody User user) {
+		return "Hello, " + user.getFirstName() + " " + user.getLastName() + " From BridgeLabz!";
+	}
+}
