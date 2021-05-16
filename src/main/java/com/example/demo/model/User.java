dcfd64d810abff59e7f59cde2d0936acfc0350c9
RuchirDixit <ruchirtd96@gmail.com>
2021-05-16 20:02:05 +0530

    [Ruchir]Added: User DTO

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+public class User {
+
+	public String firstName;
+	public String lastName;
+	
+	// Get first name of user
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	// Get last name of user
+	public String getLastName() {
+		return lastName;
+	}
+}
