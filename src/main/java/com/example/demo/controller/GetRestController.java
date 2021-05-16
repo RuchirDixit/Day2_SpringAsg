022ae5b179e8160b6ccf80841363aef6253b0c96
RuchirDixit <ruchirtd96@gmail.com>
2021-05-16 19:47:29 +0530

    [Ruchir]Added: Get API using query params

--- src/main/java/com/example/demo/controller/GetRestController.java
+++ src/main/java/com/example/demo/controller/GetRestController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,15 @@ public class GetRestController {
 	@RequestMapping(value = {"","/"})
 	public String sayHello() {
 		return "Hello From Bridgelabz!";
+	}
+	
+	@GetMapping(value = {"/query"})
+	public String printNameUsingQueryParam(@RequestParam(value = "name") String name) {
+		return "Hello, " + name + " From Bridgelabz!";
+	}
+	
+	@GetMapping(value = {"/param/{name}"})
+	public String printNameUsingPathParam(@PathVariable String name) {
+		return "Hello, " + name + " From Bridgelabz with Path!";
 	}
 }
